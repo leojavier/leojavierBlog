cacff34ea29209496d9bdbc4ca0ff34239cc6575
leojavier <info@leojavier.com>
2016-01-23 18:51:37 -0500

    updating config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // # Ghost Configuration
 // Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
-
+//
 
 var path = require('path'),
     config;
