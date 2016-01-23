6724d964ea49921b67bd5fea83cb4cfb9eb12550
leojavier <info@leojavier.com>
2016-01-23 15:09:03 -0500

    adding configuration

--- config.js
+++ config.js
@@ -14,16 +14,20 @@ config = {
         url: 'http://my-ghost-blog.com',
         mail: {},
         database: {
-            client: 'sqlite3',
+            client: 'postgres',
             connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
+              host: 'ec2-54-197-224-155.compute-1.amazonaws.com',
+              user: 'yhgelrewjabhea ',
+              password: 'Q4jXoxJBPv3uRaE77J4b9fP2je ',
+              database: 'dbihgoamfu7bo6 ',
+              port: '5432'
             },
             debug: false
         },
 
         server: {
-            host: '127.0.0.1',
-            port: '2368'
+            host: '0.0.0.0',
+            port: process.env.PORT
         }
     },
 
